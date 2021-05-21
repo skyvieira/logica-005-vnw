2bed28079c5fcc842d322800efbd33df749ef945
Ana Vieira <vieira.liviaaa@gmail.com>
2021-05-21 13:50:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let colorList = ['Vermelho', 'Verde', 'Amarelo', 'Azul', 'Branco']
+console.log(colorList)
+
+colorList.unshift('Preto')
+console.log(colorList)
+
+colorList.pop(5)
+console.log(colorList)
+
+colorList.push('Roxo', 'Rosa')
+console.log(colorList)
+
+colorList.shift(0)
+console.log(colorList)
+
+let numberList = [7, 5, 6, 3, 8, 9, 2, 1, 4]
+numberList.sort()
+console.log(numberList)
